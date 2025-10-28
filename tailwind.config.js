da19022247983956e8e870bd78c005c315a85d12
FeltSense <feltsense62@gmail.com>
2025-10-27 22:29:08 -0600

    Auto-commit: Fix deployment workflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'marquee': 'marquee 30s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
+      }
+    },
   },
   plugins: [],
 };
